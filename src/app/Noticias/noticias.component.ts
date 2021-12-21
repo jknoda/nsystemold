6bae33c386ed96ba2259cd2f39c2898b5e150f9b
Jorge Noda <jknoda@hotmail.com>
2021-12-21 10:08:20 -0300

    email news

--- src/app/Noticias/noticias.component.ts
+++ src/app/Noticias/noticias.component.ts
@@ -1,9 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
 import {  MessageService } from 'primeng/api';
 import { Subscription } from 'rxjs';
+import { MailModel } from '../model/mail.model';
 import { NewsModel } from '../model/news.model';
+import { SendmailService } from '../shared/sendmail.service';
 import { ServiceConfig } from '../_config/services.config';
 import { NewsService } from './noticias.service';
 
@@ -11,25 +13,28 @@ import { NewsService } from './noticias.service';
   selector: 'app-noticias',
   templateUrl: './noticias.component.html',
   styleUrls: ['./noticiaslista.component.css'],
-  providers: [NewsService,MessageService]
+  providers: [NewsService,MessageService,SendmailService]
 })
-export class NoticiasComponent implements OnInit {
+export class NoticiasComponent implements OnInit, OnDestroy {
   private EmpIdf: number = ServiceConfig.EMPIDF;
   private NewsIdf: number = 0;
+  mail = new MailModel();
 
   dadosForm: FormGroup;
 
   addDados: Subscription;
-  
+  sendMailSub: Subscription;  
+
   isLoading = true;
   editMode = false;
 
   constructor(private router: Router, private route: ActivatedRoute, 
-    private srvNews: NewsService, private messageService: MessageService) 
+    private srvNews: NewsService, private messageService: MessageService, private sendmailService: SendmailService) 
   {    
   }
 
   ngOnInit() {
+    this.mail = JSON.parse(localStorage.getItem('emailConfig'));
     this.route.queryParams
       .subscribe(params => {
         this.EmpIdf = params.EmpIdf;
@@ -47,10 +52,12 @@ export class NoticiasComponent implements OnInit {
       NewsTitulo: this.dadosForm.value['titulo'],
       NewsTexto: this.dadosForm.value['noticia'],
       NewsData: new Date(),
-      NewsAutor: JSON.parse(localStorage.getItem('userData')).nome
+      NewsAutor: JSON.parse(localStorage.getItem('userData')).nome,
+      NewsEmail: JSON.parse(localStorage.getItem('userData')).email
     };    
     this.addDados = this.srvNews.addNewsDados(dados).subscribe(
       () => {
+        this.sendMail(dados.NewsAutor,dados.NewsEmail,dados.NewsTexto,dados.NewsTexto);
         this.messageService.add({severity:'success', summary: 'Successo', detail: 'Notícia incluida!'});
       },
       err => { 
@@ -93,10 +100,30 @@ export class NoticiasComponent implements OnInit {
     this.messageService.clear();
   }    
 
+  sendMail(nome, email, texto, textoHtml){
+    this.mail.subject = "Nova notícia";
+    this.mail.cc = "";
+    this.mail.subject += " enviado por " + nome + " - "+email;
+    this.mail.text = "Notícia: \n" + "( " + texto +" )";
+    this.mail.html = "Notícia: <br/>" + "( " + textoHtml +" )";
+    this.sendMailSub = this.sendmailService.sendMail(this.mail).subscribe(
+      () => {
+        //this.messageService.add({severity:'success', summary: 'Successo', detail: 'Comentário excluído!'});
+      },
+      err => { 
+        let msg = err.error.errors.toString();
+        this.messageService.add({severity:'error', summary: 'Erro', detail: msg});
+      }
+    );
+  }
 
   ngOnDestroy(): void {
     if (this.addDados != null){
       this.addDados.unsubscribe();
+    }
+
+    if (this.sendMailSub != null){
+      this.sendMailSub.unsubscribe();
     }
   }
 
