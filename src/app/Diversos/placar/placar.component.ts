1686cc1903ad58596109e7c9b7ce09e422dffcfd
Jorge Noda <jknoda@hotmail.com>
2022-08-19 16:55:59 -0300

    relogio

--- src/app/Diversos/placar/placar.component.ts
+++ src/app/Diversos/placar/placar.component.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import {ConfirmationService, MessageService} from 'primeng/api';
 import { DiversosService } from '../diversos.service';
 import { Router } from '@angular/router';
+import { CdTimerComponent } from 'angular-cd-timer';
 
 @Component({
   selector: 'app-placar',
@@ -10,6 +11,8 @@ import { Router } from '@angular/router';
   providers: [MessageService,ConfirmationService,DiversosService,MessageService]
 })
 export class PlacarComponent implements OnInit {
+  @ViewChild("jikan") jikan : CdTimerComponent;
+  @ViewChild("osaekomi") osaekomi : CdTimerComponent;
 
   isLoading = false;
   ipponA = 0;
@@ -20,10 +23,35 @@ export class PlacarComponent implements OnInit {
   wazariB = 0;
   shidoB = 0;
 
+  started = false;
+
   constructor(private router: Router, private messageService: MessageService, private confirmationService: ConfirmationService) {}
 
-
-  ngOnInit(): void {
+  
+  ngOnInit(): void {    
   }
 
+  hajime(){
+    if (this.started){
+      this.jikan.resume();
+    }
+    else{
+      this.jikan.start();
+    }
+    this.started = true;
+  }
+  matte(){
+    this.jikan.stop();
+    this.osaekomi.stop();
+  }
+  osae(){
+    this.osaekomi.start();
+  }
+  toketa(){
+    this.osaekomi.stop();
+  }
+  zerar(){
+    this.jikan.reset();
+    this.osaekomi.reset();
+  }
 }
