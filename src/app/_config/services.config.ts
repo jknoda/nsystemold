876a0bcbd8328d478ccc5694749a4fd916fd804d
Jorge Noda <jknoda@hotmail.com>
2022-10-10 13:32:24 -0300

    novo link api

--- src/app/_config/services.config.ts
+++ src/app/_config/services.config.ts
@@ -1,5 +1,5 @@
 export class ServiceConfig {
-    public static API_ENDPOINT: string = "https://yamazakijudoapi.herokuapp.com";
+    public static API_ENDPOINT: string = "https://yamazakiapi.herokuapp.com";
     //public static API_ENDPOINT: string = "http://localhost:3003";
     public static EMPIDF: number = 1;
 }
